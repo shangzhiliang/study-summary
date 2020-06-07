42b1270094b65535e20bf61502acd30800f04a82
shangzhiliang <shangzhiliang@meicai.cn>
2020-06-07 17:47:20 +0800

    add
    add

---
+++ study-algorithm/src/main/java/com/study/summary/algorithm/sort/SortCollections.java
@@ -0,0 +1,12 @@
+package com.study.summary.algorithm.sort;
+
+/**
+ * @ClassName: SortCollections
+ * @Description: TODO
+ * @Auther: shangzhiliang
+ * @DATE: 2020-06-07 17:41
+ * @VERSION: V1.0
+ */
+public class SortCollections {
+
+}
